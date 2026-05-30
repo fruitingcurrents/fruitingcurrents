c003d1b18cc005f1ab44056ee380b1ac1394782b
commonspring <fsfeng@users.noreply.github.com>
2026-05-30 18:36:43 -0400

    add custom domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Replace with your actual GitHub username and repository name
-  site: "https://fruitingcurrents.github.io",
+  site: "https://fruitingcurrents.com",
   base: "/",
 });
